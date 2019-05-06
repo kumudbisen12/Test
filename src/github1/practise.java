8f47f4693bbf6738520a2b165c610cc6cd4fe03e
unknown <mb7921473@gmail.com>
2019-05-06 23:36:50 +0530

    first commit

---
+++ src/github1/practise.java
@@ -0,0 +1,18 @@
+package github1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class practise {
+
+	public static void main(String[] args) 
+	{
+		
+			  System.setProperty("webdriver.chrome.driver", "C:\\Users\\ab\\Desktop\\Testing\\chromedriver.exe");
+		     WebDriver driver  = new ChromeDriver();
+		      System.out.println("Chrome Driver Open Successfully");
+		  
+
+	}
+
+}
